a1837dae0bde26cafc95bfdd86422864a652fefe
Siddharth Gautam <siddharthgautam@Siddharths-MacBook-Air.local>
2023-10-31 22:43:01 +0530

    Adding text transition on landing page

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './Hero.css';
 import loop6 from './loop6.mp4'
+import TextTransition, { presets } from 'react-text-transition';
+
 function Hero() 
  {
+  const H5_TEXTS = ['WEB DEVELOPER', 'WRiTER', 'THiNKER'];
+  const [h5Index, setH5Index] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setH5Index((index) => index + 1),
+      2500
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+
   return (
     <div className="hero-container">
         <div className="hero-vid">
@@ -11,8 +24,17 @@ function Hero()
             </video>
         </div>
         <div className="hero-text">
-            <h1>SIDDHARTH GAUTAM</h1>
-            <h5>WEB DEVELOPER</h5>
+            <div>
+                <h1>
+                    SIDDHARTH GAUTAM
+                </h1>
+            </div>
+            <div>
+                <h5>
+                <TextTransition className='text-align'>{H5_TEXTS[h5Index % H5_TEXTS.length]}</TextTransition>
+                </h5>
+            </div>
+            
         </div>
     </div>
   );
