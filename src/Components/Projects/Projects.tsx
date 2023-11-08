cba2d9eacfd176ec728e75c8fa7c020823267b13
Siddharth Gautam <siddharth.gautam01@protonmail.com>
2023-11-08 18:47:26 +0530

    Adding parent component for Projects

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,25 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import './Projects.css'
+import './Projects.css';
+import ParticlesFloating from '../ParticlesFloating/ParticlesFloating';
+import ProjectCarousel from '../ProjectsWindow/ProjectCarousel';
 
+function BlackOverlay() {
+  return <div className="black-overlay" />;
+}
 
 function Projects() {
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
-    // When the component mounts, set a timer to hide it after a certain time (e.g., 5 seconds)
+    // When the component mounts, set a timer to hide it after a certain time (e.g., 200 milliseconds)
     const timer = setTimeout(() => {
       setVisible(true);
-    }, 200); 
+    }, 20);
 
     return () => {
       clearTimeout(timer);
     };
   }, []);
 
-
   return (
-    <div className={`projects ${visible ? 'fade-in' : ''}`}>
-      <div>LOREM IPSUM AHAHAHAHAH</div>
+    <div>
+      <div className={`projects ${visible ? 'fade-in' : ''}`}>
+        {/* TODO: Make this a carousel */}
+        <ProjectCarousel /> 
+        
+        <div>
+          <ParticlesFloating />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/ProjectsWindow/ProjectCarousel.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import './ProjectCarousel.css';
+import ParticlesFloating from '../ParticlesFloating/ParticlesFloating';
+
+
+function ProjectCarousel() {
+
+  useEffect(() => {
+    
+  }, []);
+
+return (
+    <div className="project-parent">
+        <div className='project-card'>
+            <div className='image3'>
+
+            </div>
+        </div>
+    </div>
+    );
+  
+}
+
+export default ProjectCarousel;
